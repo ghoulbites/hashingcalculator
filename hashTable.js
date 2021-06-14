5313b577ff06490596dff8ebe40f2451333c5eb3
ghoulbites <mikehanna31@gmail.com>
2021-06-14 16:01:19 +0400

    Fixed HashTable class and methods

--- hashTable.js
+++ hashTable.js
@@ -1,43 +1,53 @@
-import * as Methods from "./hashTableMethods.js"
+//* Default Texts
+const DEFAULT_DISPLAY_TEXT = '<div class="row my-1">\
+<span class="text-white">some sample text</span>\
+</div>'
+
+
+//* Page Elements
+const TABLE_DISPLAY = document.getElementById("table-display")
+
 
 
 export class HashTable {
   constructor(size = 7) {
     this.size = size
-    this.table = [12,415,6127,882,12]
-    this.count = 0
+    this.table = [14, 51, 125]
+    this.count = 2
+    this.loadFactor = 0
+  }
+
+  SetCount() {
+    this.count = this.table.length
+  }
+  SetLoadFactor() {
     this.loadFactor = this.count / this.size
   }
 
-  display = Methods.DisplayTable(this);
-  //updateLF = Methods.UpdateLoadFactor();
-  //InsertKey(key);
-  //DeleteKey(key);
-}
+
+  ClearTable() {
+    this.table = []
+    this.DisplayTable()
+  }
 
 
-/*
-const DEFAULT_DISPLAY_TEXT = '<div class="row my-1">\
-<span class="text-white">some sample text</span>\
-</div>'
+  DisplayTable() {
+    TABLE_DISPLAY.innerHTML = DEFAULT_DISPLAY_TEXT
+    if (this.count !== 0) {
+      this.table.forEach((item, index) => {
+        const TABLE_ELEMENT_ROW_CONTAINER = document.createElement("div")
+        TABLE_ELEMENT_ROW_CONTAINER.classList.add("row")
+        TABLE_ELEMENT_ROW_CONTAINER.classList.add("py-1")
 
-function DisplayTable() {
-  const Table_Display = document.getElementById("table-display")
-  if (HashTable.count === 0) {
-    Table_Display.innerHTML = DEFAULT_DISPLAY_TEXT
-  } else {
-    HashTable.table.forEach((element) => {
-      const TableIndexElementRow = document.createElement("div")
-      TableIndexElementRow.classList.add("row")
-      TableIndexElementRow.classList.add("py-1")
+        const TABLE_ELEMENT_TEXT_SPAN = document.createElement("span")
+        TABLE_ELEMENT_TEXT_SPAN.innerText = `Index ${index}: ${item}`
+        TABLE_ELEMENT_TEXT_SPAN.classList.add("text-white")
 
-      const TableIndexElementSpan = document.createElement("span")
-      TableIndexElementSpan.innerText = "Index " + i + ": " + HashTable.table[i]
-      TableIndexElementSpan.classList.add("text-white")
-
-      TableIndexElementRow.appendChild(TableIndexElementSpan)
-      Table_Display.appendChild(TableIndexElementRow)
-    })
+        TABLE_ELEMENT_ROW_CONTAINER.appendChild(TABLE_ELEMENT_TEXT_SPAN)
+        TABLE_DISPLAY.appendChild(TABLE_ELEMENT_ROW_CONTAINER)
+      })
+    }
   }
 }
-*/
+
+
--- script.js
+++ script.js
@@ -3,4 +3,17 @@ import {HashTable} from "./hashTable.js"
 let newHashTable = new HashTable();
 
 console.log(newHashTable.size);
-console.log(newHashTable.display());
+
+newHashTable.DisplayTable()
+
+console.log(newHashTable.count);
+
+newHashTable.table = [12, 41, 676];
+
+console.log(newHashTable.size);
+
+newHashTable.DisplayTable()
+
+console.log(newHashTable.count);
+
+newHashTable.ClearTable();
